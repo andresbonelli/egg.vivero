387d1175a6084ca5f3fe1e3f7b2a9ffe44a2d761
andresbonelli <andresbonellipiano@gmail.com>
2025-02-03 21:00:36 -0300

    DAO: Listar  productos

--- vivero/src/main/java/com/egg/vivero/entities/Producto.java
+++ vivero/src/main/java/com/egg/vivero/entities/Producto.java
@@ -1,8 +1,17 @@
 package com.egg.vivero.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Producto {
     private int idProducto;
-    private int codigoProducto;
+    private String codigoProducto;
     private String nombre;
     private int idGama;
     private String dimensiones;
@@ -11,4 +20,16 @@ public class Producto {
     private int cantidadEnStock;
     private double precioVenta;
     private double precioProveedor;
+
+    public Producto(String codigoProducto, String nombre, int idGama, String dimensiones, String proveedor, String descripcion, int cantidadEnStock, double precioVenta, double precioProveedor) {
+        this.codigoProducto = codigoProducto;
+        this.nombre = nombre;
+        this.idGama = idGama;
+        this.dimensiones = dimensiones;
+        this.proveedor = proveedor;
+        this.descripcion = descripcion;
+        this.cantidadEnStock = cantidadEnStock;
+        this.precioVenta = precioVenta;
+        this.precioProveedor = precioProveedor;
+    }
 }
---
+++ vivero/src/main/java/com/egg/vivero/persistence/ProductoDAO.java
@@ -0,0 +1,57 @@
+package com.egg.vivero.persistence;
+
+import com.egg.vivero.entities.Producto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoDAO extends DAO{
+    public Producto buscarProductoPorId(int id) throws Exception {
+        consultarDataBase("SELECT * FROM producto WHERE id_producto = " + id);
+        Producto producto = new Producto();
+        if (resultSet.next()) {
+            producto.setIdProducto(resultSet.getInt("id_producto"));
+            producto.setCodigoProducto(resultSet.getString("codigo_producto"));
+            producto.setNombre(resultSet.getString("nombre"));
+            producto.setIdGama(resultSet.getInt("id_gama"));
+            producto.setDimensiones(resultSet.getString("dimensiones"));
+            producto.setProveedor(resultSet.getString("proveedor"));
+            producto.setDescripcion(resultSet.getString("descripcion"));
+            producto.setCantidadEnStock(resultSet.getInt("cantidad_en_stock"));
+            producto.setPrecioVenta(resultSet.getDouble("precio_venta"));
+            producto.setPrecioProveedor(resultSet.getDouble("precio_proveedor"));
+        }
+        desconectarDataBase();
+        return producto;
+    }
+
+    public void guardarProducto(Producto producto) throws Exception {
+        insertarModificarEliminarDataBase("INSERT INTO producto (codigo_producto, nombre, id_gama, dimensiones, proveedor, descripcion, cantidad_en_stock, precio_venta, precio_proveedor) VALUES ('" + producto.getCodigoProducto() + "', '" + producto.getNombre() + "', " + producto.getIdGama() + ", '" + producto.getDimensiones() + "', '" + producto.getProveedor() + "', '" + producto.getDescripcion() + "', " + producto.getCantidadEnStock() + ", " + producto.getPrecioVenta() + ", " + producto.getPrecioProveedor() + ")");
+        desconectarDataBase();
+    }
+
+    public List<Producto> listarProductos() throws Exception {
+        consultarDataBase("SELECT * FROM producto");
+        List<Producto> productos = new ArrayList<>();
+        while (resultSet.next()) {
+            productos.add(new Producto(
+                    resultSet.getInt("id_producto"),
+                    resultSet.getString("codigo_producto"),
+                    resultSet.getString("nombre"),
+                    resultSet.getInt("id_gama"),
+                    resultSet.getString("dimensiones"),
+                    resultSet.getString("proveedor"),
+                    resultSet.getString("descripcion"),
+                    resultSet.getInt("cantidad_en_stock"),
+                    resultSet.getDouble("precio_venta"),
+                    resultSet.getDouble("precio_proveedor")
+            ));
+        }
+        desconectarDataBase();
+        return productos;
+    }
+
+}
+
